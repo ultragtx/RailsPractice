e3e9fd34755074110997ee0309b6e461ffee021f
ultragtx <ultragtx@gmail.com>
2013-04-26 21:20:09 +0800

    Chap 7

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -3,6 +3,12 @@ require 'test_helper'
 class ProductsControllerTest < ActionController::TestCase
   setup do
     @product = products(:one)
+    @update = {
+      title: 'Lorem Ipsum',
+      description: 'Wibbles are fun!',
+      image_url: 'lorem.jpg',
+      price: 19.95
+    }
   end
 
   test "should get index" do
@@ -18,7 +24,7 @@ class ProductsControllerTest < ActionController::TestCase
 
   test "should create product" do
     assert_difference('Product.count') do
-      post :create, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+      post :create, product: @update
     end
 
     assert_redirected_to product_path(assigns(:product))
@@ -35,7 +41,7 @@ class ProductsControllerTest < ActionController::TestCase
   end
 
   test "should update product" do
-    put :update, id: @product, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+    put :update, id: @product, product: @update
     assert_redirected_to product_path(assigns(:product))
   end
 
@@ -46,4 +52,26 @@ class ProductsControllerTest < ActionController::TestCase
 
     assert_redirected_to products_path
   end
+  
+  test "product is not valid without a unique title" do
+      product = Product.new(title:       products(:ruby).title,
+                            description: "yyy", 
+                            price:       1, 
+                            image_url:   "fred.gif")
+
+      assert product.invalid?
+      assert_equal ["has already been taken"], product.errors[:title]
+  end
+  
+  test "product is not valid without a unique title - i18n" do
+      product = Product.new(title:       products(:ruby).title,
+                            description: "yyy", 
+                            price:       1, 
+                            image_url:   "fred.gif")
+
+      assert product.invalid?
+      assert_equal [I18n.translate('activerecord.errors.messages.taken')],
+                   product.errors[:title]
+  end
+  
 end
